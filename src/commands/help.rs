e929ec50a33f03aadd41cd7f654b64bc5f0c6ae6
baum <lorencalebbaum@gmail.com>
2020-11-21 11:57:34 -0500

    got the author

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -1,4 +1,5 @@
 use serenity::prelude::*;
+use serenity::http::*;
 use serenity::model::prelude::*;
 use serenity::framework::standard::{
     Args, CommandResult,
@@ -6,7 +7,15 @@ use serenity::framework::standard::{
 };
 
 #[command]
-pub async fn help(ctx: &Context, msg: &Message) -> CommandResult {
+pub async fn help(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+
+    
+    if args.is_empty() {
+        
+        msg.channel_id.say(&ctx.http, format!("yes this is help, {}. type !help commands", msg.author)).await?;
+    }
+    // match args {
+    // };
     msg.channel_id.say(&ctx.http, "yes this is help").await?;
 
     Ok(())
