325395f7cfafcffa2c585bebc3fc42e8374b8f1b
baum <lorencalebbaum@gmail.com>
2020-11-22 06:37:58 -0500

    Trying out the default help command

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -1,9 +1,10 @@
+use std::collections::HashSet;
 use serenity::prelude::*;
 use serenity::http::*;
 use serenity::model::prelude::*;
 use serenity::framework::standard::{
-    Args, CommandResult,
-    macros::command,
+    Args, CommandResult, HelpOptions, help_commands, CommandGroup,
+    macros::{group, command, help},
 };
 
 const COMMANDS: [&str; 6] = [
@@ -15,15 +16,45 @@ const COMMANDS: [&str; 6] = [
     "kappa",
 ];
 
-#[command]
-pub async fn help(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+// #[command]
+// pub async fn help(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
 
-    let reply_message = match args.len() {
-        0 => format!("yes this is help, {}. type !help commands list of commands", msg.author),
-        1 => args.message().to_string(),
-        _ => format!("yes this is help, {}. only one argument at a time plz. type !help commands for list of commands", msg.author),
-    };
+//     let reply_message = match args.len() {
+//         0 => format!("yes this is help, {}. type !help commands for list of commands", msg.author),
+//         1 => args.message().to_string(),
+//         _ => format!("yes this is help, {}. only one argument at a time plz. type !help commands for list of commands", msg.author),
+//     };
 
-    msg.channel_id.say(&ctx.http, reply_message).await?;
+//     msg.channel_id.say(&ctx.http, reply_message).await?;
+//     Ok(())
+// }
+
+// The framework provides two built-in help commands for you to use.
+// But you can also make your own customized help command that forwards
+// to the behaviour of either of them.
+#[help]
+// This replaces the information that a user can pass
+// a command-name as argument to gain specific information about it.
+#[individual_command_tip =
+"Hello! こんにちは！Hola! Bonjour! 您好!\n\
+If you want more information about a specific command, just pass the command as argument."]
+// Some arguments require a `{}` in order to replace it with contextual information.
+// In this case our `{}` refers to a command's name.
+#[command_not_found_text = "Could not find: `{}`."]
+// Define the maximum Levenshtein-distance between a searched command-name
+// and commands. If the distance is lower than or equal the set distance,
+// it will be displayed as a suggestion.
+// Setting the distance to 0 will disable suggestions.
+#[max_levenshtein_distance(3)]
+
+async fn my_help(
+    context: &Context,
+    msg: &Message,
+    args: Args,
+    help_options: &'static HelpOptions,
+    groups: &[&'static CommandGroup],
+    owners: HashSet<UserId>
+) -> CommandResult {
+    let _ = help_commands::with_embeds(context, msg, args, help_options, groups, owners).await;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,18 @@ use std::{
 use serenity::{
     async_trait,
     client::bridge::gateway::ShardManager,
-    framework::{
-        StandardFramework,
-        standard::macros::group,
+    framework::standard::{
+        StandardFramework, HelpOptions, help_commands, CommandGroup,
+        macros::{group, command, help},
     },
     http::Http,
-    model::{event::ResumedEvent, gateway::Ready},
+    model::{
+        channel::{Channel, Message},
+        gateway::Ready,
+        id::UserId,
+        permissions::Permissions,
+        event::ResumedEvent
+    },
     prelude::*,
 };
 
@@ -45,8 +51,37 @@ impl EventHandler for Handler {
 }
 
 #[group]
-#[commands(help)]
 struct General;
+
+// // The framework provides two built-in help commands for you to use.
+// // But you can also make your own customized help command that forwards
+// // to the behaviour of either of them.
+// #[help]
+// // This replaces the information that a user can pass
+// // a command-name as argument to gain specific information about it.
+// #[individual_command_tip =
+// "Hello! こんにちは！Hola! Bonjour! 您好!\n\
+// If you want more information about a specific command, just pass the command as argument."]
+// // Some arguments require a `{}` in order to replace it with contextual information.
+// // In this case our `{}` refers to a command's name.
+// #[command_not_found_text = "Could not find: `{}`."]
+// // Define the maximum Levenshtein-distance between a searched command-name
+// // and commands. If the distance is lower than or equal the set distance,
+// // it will be displayed as a suggestion.
+// // Setting the distance to 0 will disable suggestions.
+// #[max_levenshtein_distance(3)]
+
+// async fn my_help(
+//     context: &Context,
+//     msg: &Message,
+//     args: Args,
+//     help_options: &'static HelpOptions,
+//     groups: &[&'static CommandGroup],
+//     owners: HashSet<UserId>
+// ) -> CommandResult {
+//     let _ = help_commands::with_embeds(context, msg, args, help_options, groups, owners).await;
+//     Ok(())
+// }
 
 #[tokio::main]
 async fn main() {
@@ -86,6 +121,7 @@ async fn main() {
         .configure(|c| c
                    .owners(owners)
                    .prefix("!"))
+        .help(&MY_HELP)
         .group(&GENERAL_GROUP);
 
     let mut client = Client::builder(&token)
